60c66d336230595752eefb91ffe39ea2d0dcc1e5
Anirudh Anil Ojha <aniojha@Anirudhs-MacBook-Pro.local>
2019-12-11 20:02:23 +0530

    Updates

---
+++ precache-manifest.5186fed434e5381f30c8ed8328b1ddcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e41cbf03336d5e172a468b58a2f1f8",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "12e9136369941b1fa1f0",
+    "url": "/homepage/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "a05566533cb120fa4e40",
+    "url": "/homepage/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "12e9136369941b1fa1f0",
+    "url": "/homepage/static/js/2.a6e496f7.chunk.js"
+  },
+  {
+    "revision": "a05566533cb120fa4e40",
+    "url": "/homepage/static/js/main.7123f303.chunk.js"
+  },
+  {
+    "revision": "9f75ca5555434e3c08ed",
+    "url": "/homepage/static/js/runtime-main.b1add913.js"
+  }
+]);
