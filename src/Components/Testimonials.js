ec5b6d99cef59b946f324db03a297b77f02c10dc
AniOjha <33929284+AniOjha@users.noreply.github.com>
2019-12-12 18:46:10 +0530

    Update Testimonials.js

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -30,8 +30,17 @@ class Testimonials extends Component {
 
             <div className="ten columns flex-container">
                   <ul className="slides">
-                      // {testimonials}
-                  </ul>
+                    <li>
+                      <blockquote>
+                         <p>..it's hard to stay mad, when there's so much beauty in the world. Sometimes I feel like I'm seeing it all at once, and it's too much, my heart fills up like a balloon that's about to burst... And then I remember to relax, and stop trying to hold on to it, and then it flows through me like rain and I can't feel anything but gratitude for every single moment of my stupid little life... You have no idea what I'm talking about, I'm sure. But don't worry... you will someday.</p>
+                         <cite>American Beauty</cite>
+                      </blockquote>
+                      <blockquote>
+                         <p>Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.</p>
+                         <cite>Micheal Scott</cite>
+                      </blockquote>            
+                  </li>
+                 </ul>
                </div>
             </div>
          </div>
